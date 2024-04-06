d333102d4f02c57ab81c24883252b1c961c3adcd
--get <charrydiego16@gmail.com>
2024-04-06 14:10:08 -0500

    it's possible to enter a match and calculate winner and won amount with visitant or winner, tie not yet available

--- sports_bets.py
+++ sports_bets.py
@@ -34,23 +34,43 @@ class House:
 
 
 class Gambler:
-  def __init__(self, name, money, broke, house):
+  def __init__(self, name, money, broke, house, bets, record):
     self.name = name
     self.money = money
     self.broke = broke
     self.house = house
+    self.bets = bets
+    self.record = record
   
-  def chooseMatch(self, match):
+  def chooseMatchTeamMoney(self, match, result, respos, money):
+    gambling = []    
     if match in self.house.bets:
-       return match
+       gambling.append(match)
+       gambling.append(result)
+       gambling.append(respos)
+       gambling.append(money)
+       self.bets = gambling
+       return self.bets
     else:
        print("Match not found")
 
-  def placeWage():
-    pass
+  def checkResults(self, match):
+    # if "team chosen" = "winner team in list"
+    if self.bets[1] == self.house.bets[match][3]:
+      # bug always calculating the visitant quote for the winner
+      won = (self.house.bets[match][self.bets[2]] * self.bets[3]) - self.bets[3]
+      self.money += won
+      self.bets = []
+      print("You won: " + str(won))
+    else:
+      self.money -= self.bets[3]
+      self.bets = []
+      print("You lost: " + str(self.bets[3]))
+
+     
+     
   
-  def chooseOutcome():
-    pass
+  
 
 betplay = House("betplay", "1000000000", {})
 betplay.addMatch("Crystal Palace vs Manchester City", [10.96, 5.48, 1.28])
@@ -63,5 +83,27 @@ print(betplay.bets)
 betplay.setWinner("Crystal Palace vs Manchester City", "Manchester City")
 print(betplay.bets)
 
-diego = Gambler("Diego", 1000000, False, betplay)
-diego.chooseMatch("Crystal Palace vs Manchester City")
+diego = Gambler("Diego", 1000, False, betplay, [], [])
+# diego.chooseMatch("Crystal Palace vs Manchester City")
+print("Important test ")
+diego.chooseMatchTeamMoney("Crystal Palace vs Manchester City", "Manchester City", 2, 100)
+#diego.chooseMatchTeamMoney("Manchester United vs Liverpool", 2, 100)
+print(diego.bets)
+print("Check results function on gambler")
+diego.checkResults("Crystal Palace vs Manchester City")
+
+print("----------second match--------")
+# tests adding a second match
+betplay.addMatch("Brighton vs Arsenal", [5.94, 4.45, 1.48])
+betplay.setWinner("Brighton vs Arsenal", "Arsenal")
+diego.chooseMatchTeamMoney("Brighton vs Arsenal", "Arsenal", 2, 100)
+diego.checkResults("Brighton vs Arsenal")
+print("Account balance: " + str(diego.money))
+
+print("----------third match--------")
+betplay.addMatch("PSG vs Clermont Foot", [1.33, 5.51, 8.01])
+diego.chooseMatchTeamMoney("PSG vs Clermont Foot", "PSG", 0, 181)
+betplay.setWinner("PSG vs Clermont Foot", "PSG")
+diego.checkResults("PSG vs Clermont Foot")
+print("Account balance: " + str(diego.money))
+
